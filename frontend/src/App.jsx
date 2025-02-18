6cbec51aaab09d6802dfad139bf361a3132119bb
HungNguyen0210 <hung.nlm03@gmail.com>
2025-02-18 17:40:18 +0700

    Basic layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import News from "./pages/News";
 import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* Route cho Login */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Route chính cho trang Home, Menu, News với MainLayout */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="news" element={<News />} />
+          <Route path="menu" element={<Menu />} />
+        </Route>
       </Routes>
     </Router>
   );
